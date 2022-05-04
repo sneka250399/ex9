e5ff6a010951ad16377138489d4765acfe49e0d1
9578686280 <aswinidmerode@gmail.com>
2022-05-04 11:09:39 +0530

    check

---
+++ ex4.java
@@ -0,0 +1,23 @@
+//ex4 github check
+
+
+import java.util.Scanner;
+
+class ex4
+{
+public static void main(String c[])
+{
+System.out.println("Good Morning Sneka");
+
+Scanner scan=new Scanner(System.in);
+
+System.out.println("Enter your name");
+String name=scan.nextLine();
+System.out.println("Enter your rollno");
+int rollno=scan.nextInt();
+System.out.println("Hi "+name);
+System.out.println("your rollno : "+rollno);
+
+
+}
+}
