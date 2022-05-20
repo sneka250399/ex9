bc6684e1a815d755862a97f9def15702bbe011a8
9578686280 <aswinidmerode@gmail.com>
2022-05-20 11:32:59 +0530

    checking process

---
+++ exfor.java
@@ -0,0 +1,37 @@
+
+#for checking process.
+
+class exfor
+
+{
+public static void main(String ar[])
+{
+
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(j);
+}
+System.out.println();
+}
+
+
+
+/*
+5
+45
+345
+2345
+12345
+*/
+
+
+
+
+
+
+}
+}
+
+
